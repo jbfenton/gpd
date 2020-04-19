02774ce5d7363413d87f1b69bdd55c30d7fe242d
jbfenton <josh@jbfenton.dev>
2020-04-19 22:42:34 +1000

    Added missing docstrings + typing to models

--- gpd/model.py
+++ gpd/model.py
@@ -4,6 +4,10 @@ Model definitions.
 
 
 class MediaItemBase:
+    """
+    Media item base definition.
+    """
+
     def __init__(self, identifier: str, filename: str, mime_type: str):
         """
         Instantiate MediaItem.
@@ -33,20 +37,38 @@ class MediaItemBase:
 
     @property
     def filename(self) -> str:
+        """
+        Retrieve media item filename.
+
+        :return: Media item filename.
+        :rtype: str
+        """
+
         return self._filename
 
     @property
     def mime_type(self) -> str:
+        """
+        Retrieve media item mime type.
+
+        :return: Media item mime type.
+        :rtype: str
+        """
+
         return self._mime_type
 
 
 class MediaItem(MediaItemBase):
+    """
+    Media item definition.
+    """
+
     def __init__(self, media_item: dict):
         """
         Instantiate MediaItem.
 
-        :param media_item:
-        :type media_item:
+        :param media_item: Dictionary representation of a media item.
+        :type media_item: dict
         """
 
         super(MediaItem, self).__init__(
@@ -57,26 +79,73 @@ class MediaItem(MediaItemBase):
 
 
 class AlbumBase:
+    """
+    Album base definition.
+    """
+
     def __init__(self, identifier: str, media_count: str, title: str):
+        """
+        Instantiate Album.
+
+        :param identifier: Album identifier.
+        :type identifier: str
+        :param media_count: Album media item count.
+        :type media_count: str
+        :param title: Album title.
+        :type title: str
+        """
+
         self._identifier = identifier
         self._media_count = int(media_count)
         self._title = title
 
     @property
-    def identifier(self):
+    def identifier(self) -> str:
+        """
+        Retrieve album identifier.
+
+        :return: Album identifier
+        :rtype: str
+        """
+
         return self._identifier
 
     @property
-    def media_count(self):
+    def media_count(self) -> int:
+        """
+        Retrieve album media count.
+
+        :return: Album media count.
+        :rtype: int
+        """
+
         return self._media_count
 
     @property
-    def title(self):
+    def title(self) -> str:
+        """
+        Retrieve album title.
+
+        :return: Album title.
+        :rtype: str
+        """
+
         return self._title
 
 
 class Album(AlbumBase):
+    """
+    Album definition.
+    """
+
     def __init__(self, album: dict):
+        """
+        Instantiate Album.
+
+        :param album: Dictionary representation of an Album.
+        :type album: dict
+        """
+
         super(Album, self).__init__(
             identifier=album['id'],
             media_count=album['mediaItemsCount'],
